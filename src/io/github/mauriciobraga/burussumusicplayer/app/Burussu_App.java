f9dee1428312f58000e0d7a1fb099b1524cdd16a
Mauricio Braga <braga.mauricio@gmail.com>
2026-07-12 21:48:21 -0300

    updated app title.

--- src/io/github/mauriciobraga/burussumusicplayer/app/Burussu_App.java
+++ src/io/github/mauriciobraga/burussumusicplayer/app/Burussu_App.java
@@ -5,7 +5,7 @@ import io.github.mauriciobraga.burussumusicplayer.view.BurussuView;
 
 public class Burussu_App {
     public static void main(String[] args) {
-        BurussuView view = new BurussuView("Burussu MVC Music Player 0.0012");
+        BurussuView view = new BurussuView("Burussu Music Player 0.0012");
         new BurussuController(view);
     }
 }
